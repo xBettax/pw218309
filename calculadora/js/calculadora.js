55dd5c549ff3dc407d0fdc29e2c00809e4ac4a11
CCDM01 <ccdm01@CCDM01.local>
2018-11-01 10:02:09 -0700

    Calculadora 01 nov 18

--- calculadora/js/calculadora.js
+++ calculadora/js/calculadora.js
@@ -1,6 +1,41 @@
-		var colorAmarillo = function(x){
-			x.style.background="yellow";
+		window.onload= function(){
+		var operador="";
+
+			var colorAmarillo = function(){
+			this.style.background="yellow";
+			}
+			
+			var colorBlanco = function(){
+			this.style.background="white";
+			}
+
+			var operando1= document.getElementById("operando1");
+			operando1.addEventListener("focus", colorAmarillo);
+			operando1.addEventListener("focusout",colorBlanco);
+		
+			var operando2=document.getElementById("operando2");
+			operando2.addEventListener("focus", colorAmarillo);
+			operando2.addEventListener("focusout",colorBlanco);
+
+			var resultado=document.getElementById("resultado");
+			resultado.addEventListener("focus", colorAmarillo);
+			resultado.addEventListener("focusout",colorBlanco);	
+		
+			var numeros=function() {
+				var valor= this.value;
+				if(operador==""){
+					var valorInput=document.getElementById("operando1").value="";
+					if(operador=="0"){
+						document.getElementById("operando1").value="";
+						document.getElementById("operando1").value+=valor;
+					}
+				}
+				else(operador=="0"){
+						document.getElementById("operando2").value="";
+						document.getElementById("operando2").value+=valor;
+			// body...
+			}
+
 		}
-		var colorBlanco = function(x){
-			x.style.background="white";
-		}
+
+		
